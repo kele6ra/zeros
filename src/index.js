40cacec5a5d33ec6004a3f4bf65ddc2d54b09fce
Andrey Ovchinnikov <ovchinnikov@strim-tech.com>
2018-02-22 12:39:48 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 module.exports = function getZerosCount(number) {
-	var i,div=0,zeros = 0;
-  for (i=0;i<=number;i=i+5){
-      div =i;
-      while (div>=5){
-          if (div%5==0){zeros++;}
-          div=div/5;
-      } 
-   }
+  var i,div=0,zeros = 0;
+  while(number){
+    number = (number/5)|0;
+    zeros+=number;
+  }
   return(zeros);
 }
